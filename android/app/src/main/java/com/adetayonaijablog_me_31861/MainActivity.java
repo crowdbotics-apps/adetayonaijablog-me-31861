45c6255d493a2cd18fb8d19f300fcb53440bf5b0
Crowdbotics <team@crowdbotics.com>
2021-11-07 10:48:02 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/adetayonaijablog_me_31861/MainActivity.java
@@ -0,0 +1,15 @@
+package com.adetayonaijablog_me_31861;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "adetayonaijablog_me_31861";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "adetayonaijablog_me_31861",
+  url: "https://adetayonaijablog_me_31861.botics.co",
+  api: "https://adetayonaijablog_me_31861.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
